d3decf5d27008fc27cbb4d4a37f6f0ca050bc8ca
Unknown <luis.d.simoes@tecnico.ulisboa.pt>
2018-12-04 11:01:32 +0000

    master
    working on user

--- Server/building.py
+++ Server/building.py
@@ -1,9 +1,10 @@
 class Building:
-    def __init__(self, b_id, name, latitude, longitude):
+    def __init__(self, b_id, name, latitude, longitude, range):
         self.id = b_id
         self.name = name
         self.latitude = latitude
         self.longitude = longitude
+        self.range = range
 
     def __str__(self):
-        return "%d - %s - %s - %s" % (self.id, self.name, self.latitude, self.longitude)
+        return "%d - %s - %s - %s - %d" % (self.id, self.name, self.latitude, self.longitude, self.range)
